ee8f1024b2832b7e29d9d1d74852adf12fc0d578
Daniel Teixeira <DanielRTeixeira@users.noreply.github.com>
2017-07-31 17:55:42 +0100

    Create run10.sh

---
+++ run10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python smb3.py 0 &
+python smb3.py 1000 &
+python smb3.py 2000 &
+python smb3.py 3000 &
+python smb3.py 4000 &
+python smb3.py 5000 &
+python smb3.py 6000 &
+python smb3.py 7000 &
+python smb3.py 8000 &
+python smb3.py 9000 &
